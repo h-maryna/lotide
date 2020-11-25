8e927a808df47c8fd1da0fed79f7cec8f246cfe5
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-25 02:14:49 +0000

    Function for comparing 2 arrays with new updates added

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,16 @@
-/**const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed:` +  `[actual]` === `[expected]`);
   } else {
     console.log(`Assertion Failed: ` + `[actual]` !== '[expected]');
-  }
+  }return assertEqual;
 
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 
-assertEqual(1, 1);**/
-const eqArrays = function(arr1, arr2) {
+assertEqual(1, 1);
+
+/**const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++){
     for (let j = 0; j < arr2.length; j++){
       if(arr1[i] === arr2[j] && arr1.length === arr2.length){
@@ -19,10 +20,24 @@ const eqArrays = function(arr1, arr2) {
       }
     }
   }
-};
+};**/
+function eqArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+      for (var i = 0; i < arr1.length; i++) {
+          if (arr1[i] !== arr2[i]) {
+              return false;
+          }
+      }
+      return true;
+  } else {
+      return false;
+  }
+}
+
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
