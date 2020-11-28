3141119cf13aaa65d9c5ecf7011d138b81421596
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-28 02:38:47 +0000

    CountLetters function added

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+
+const countLetters = function (str, letter){
+  let letter_Count = 0;
+  for (let position = 0; position < str.length; position++) {
+    if (str.charAt(position) == letter) {
+      letter_Count += 1;
+    }
+  }
+  return letter_Count;
+};
+
+console.log(countLetters("Lighthouse house ", "h"));
+
+
