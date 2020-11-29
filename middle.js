30031665d366eecfd1f85c91b4feb34ae5a3e9a8
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-29 04:54:07 +0000

    Middle function was added

---
+++ middle.js
@@ -0,0 +1,25 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+const isOdd = function (num){
+  return (num % 2 != 0);
+};
+const hasMiddle = function (arr){
+  return !(arr.length === 1 || arr.length === 2);
+};
+const middle = function(arr){
+  let res = [];
+  if (hasMiddle(arr)){
+    let midIndex = Math.floor(arr.length/2) - 1;
+    if (isOdd(arr.length)){
+      res = arr[midIndex + 1];
+    }else{
+      res.push(arr[midIndex]);
+      res.push(arr[midIndex + 1]);
+    }
+  }
+  return res;
+};
+
+module.exports = eqArrays;
+module.exports = assertArraysEqual;
