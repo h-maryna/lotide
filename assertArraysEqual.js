6b5d019ef2133b38ad91dd09aaef9165fcaa390e
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-29 23:22:19 +0000

    Updated assertArraysEqual.js was added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,11 @@
-/**const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:` +  `[actual]` === `[expected]`);
-  } else {
-    console.log(`Assertion Failed: ` + `[actual]` !== '[expected]');
-  }return assertEqual;
+const eqArrays = require('./eqArrays');
 
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-
-assertEqual(1, 1);**/
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-      for (var i = 0; i < arr1.length; i++) {
-          if (arr1[i] !== arr2[i]) {
-            console.log(`Assertion Failed: `);
-          }
-      }
-      console.log(`Assertion Passed:`);
-  } else {
-    console.log(`Assertion Failed: ` );
-  }
-}
+const assertArraysEqual = function(arr1, arr2){
+    if(eqArrays(arr1, arr2)){
+      console.log(`Assertion Passed: [${arr1}] === [${arr2}].`);
+    }else console.log(`Assertion Failed: [${arr1}] !== [${arr2}].`);
+  };
 
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
+module.exports = assertArraysEqual;
