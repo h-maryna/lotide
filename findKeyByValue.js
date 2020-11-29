93c8bb3e0475ec93cb1d1524744e127d7fd80774
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-29 04:33:44 +0000

    FindKeyByValue was added

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value){
+  let keys = Object.keys(obj);
+  for (let k of keys){
+    if(obj[k] === value){
+      return k;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
