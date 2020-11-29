daeb7bd3226af57dba5cdac0fea6433540ab56f9
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-29 22:58:05 +0000

    Updated head.js and assertEqual.js were added

--- head.js
+++ head.js
@@ -1,20 +1,10 @@
-let arr = [];
-function head(arr){
-  return arr[0];
+const assertEqual = require('./assertEqual');
+
+
+const head = function(array){
+  return array[0];
 }
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:` +  `[actual]` === `[expected]`);
-  } else if(actual !== expected){
-    console.log(`Assertion Failed: ` + `[actual]` !== '[expected]');
-  } else if(actual === [] || expected === []){
-    console.log(`undefined`);
-  }
-};
 
-assertEqual("Lighthouse Labs", "Bootcamp");
 
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7], 5));
-assertEqual(head(["Head", "Lighthouse", "Labs"]), "Hello");
+module.exports = assertEqual;
+module.exports = head;
