74858a760393d9e0f02f8cf187e4ddd381901e90
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-29 23:10:42 +0000

    Updated eqArrays.js was added

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length) {
+  if(arr1.length != arr2.length){
     return false;
-  }
-  for ( let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  }else{
+    for (let i = 0; i < arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
     }
   }
   return true;
-}
+};
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = assertEqual;
+module.exports = eqArrays;
